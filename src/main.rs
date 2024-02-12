66b38d9bdbbe4c508cea6799ffef1a90428a90d9
Ikaia <ileleiwi@gmail.com>
2024-02-12 14:33:08 -0700

    output directory and unique function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use clap::{Command, Arg};
 use bio::io::fasta;
 use bio::alignment::sparse::hash_kmers;
+use std::fs;
 use std::str::FromStr;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs::File;
-use std::io::BufReader;
+use std::fs::create_dir_all;
 use std::io::Write;
+use std::io::Error;
+use std::path::PathBuf;
 
 
 fn main() {
@@ -28,12 +31,20 @@ fn main() {
         .short('s')
         .long("shared_out")
         .value_name("FILE")
-        .help("Path for shared output file"))
+        .help("Name of shared output file")
+        .required(false))
     .arg(Arg::new("unique_out")
         .short('u')
         .long("unique_out")
         .value_name("FILE")
-        .help("Path for unique output file"))
+        .help("Name of unique output file")
+        .required(false))
+    .arg(Arg::new("output_dir")
+        .short('o')
+        .long("output_dir")
+        .value_name("DIRECTORY")
+        .help("Output directory")
+        .required(false))
     .arg(Arg::new("kmer_size")
         .short('k')
         .long("kmer_size")
@@ -52,15 +63,29 @@ let fasta_file2 = matches.get_one::<String>("fasta2").unwrap_or_else(|| {
     std::process::exit(1);
 });
 // Use default output file names if not provided
+let default_output_dir = String::from("./kmer_search_output");
+let output_dir = matches
+    .get_one::<String>("output_dir")
+    .map_or(PathBuf::from(&default_output_dir), |dir| PathBuf::from(dir));
+
+// Create output directory if it doesn't exist
+match fs::create_dir_all(&output_dir) {
+    Ok(_) => println!("Output directory created"),
+    Err(e) => {println!("Error creating output directory: {}", e);},
+}
+
 let default_shared_out = String::from("shared.tsv");
 let shared_out = matches
     .get_one::<String>("shared_out")
     .unwrap_or(&default_shared_out);
+let shared_out_path = output_dir.join(shared_out);
 
 let default_unique_out = String::from("unique.tsv");
 let unique_out = matches
     .get_one::<String>("unique_out")
     .unwrap_or(&default_unique_out);
+let unique_out_path = output_dir.join(unique_out);
+
 
 // Retrieve the value of the kmer_size argument
 let kmer_size_str = matches.get_one::<String>("kmer_size").unwrap();
@@ -88,7 +113,7 @@ let kmer_keys2 = kmer_counts2.keys().cloned().collect::<HashSet<_>>();
 // Intersect the cloned key sets
 let shared_kmers: HashSet<String> = kmer_keys1.intersection(&kmer_keys2).cloned().collect();
 
-let mut shared_file = File::create(shared_out).unwrap();
+let mut shared_file = File::create(shared_out_path).unwrap();
 writeln!(shared_file, "kmer\tfasta1_count\tfasta2_count").unwrap();
 for kmer in &shared_kmers {
     let count1 = kmer_counts1.get(kmer).unwrap_or(&0);
@@ -108,33 +133,17 @@ for kmer in &shared_kmers {
 let unique1: Vec<String> = kmer_keys1.difference(&kmer_keys2).cloned().collect();
 let unique2: Vec<String> = kmer_keys2.difference(&kmer_keys1).cloned().collect();
 
-let mut unique_file = File::create(unique_out).unwrap();
+let mut unique_file = File::create(unique_out_path).unwrap();
 writeln!(unique_file, "kmer\tfasta1_count\tfasta2_count").unwrap();
-for kmer in &unique1 {
-    let count = kmer_counts1[kmer];
-    match writeln!(unique_file, "{}\t{}\t{}", kmer, count, 0) {
-        Ok(_) => {
-            // Do nothing
-        },
-        Err(e) => {
-            eprintln!("Error writing to unique file: {}", e);
-            std::process::exit(1);
-        }
-    };
+match find_unique_kmers(&unique1, &kmer_counts1, &mut unique_file) {
+    Ok(()) => println!("Unique kmers from fasta1 written to file"),
+    Err(e) => eprintln!("Error writing unique kmers to file: {}", e),
+}
+match find_unique_kmers(&unique2, &kmer_counts2, &mut unique_file) {
+    Ok(()) => println!("Unique kmers from fasta2 written to file"),
+    Err(e) => eprintln!("Error writing unique kmers to file: {}", e),
 }
 
-for kmer in &unique2 {
-    let count = kmer_counts2[kmer];
-    match writeln!(unique_file, "{}\t{}\t{}", kmer, 0, count) {
-        Ok(_) => {
-            // Do nothing
-        },
-        Err(e) => {
-            eprintln!("Error writing to unique file: {}", e);
-            std::process::exit(1);
-        }
-    };
-}
 }
 
 fn count_kmers(fasta_file: &str, k: usize) -> HashMap<String, usize> {
@@ -166,3 +175,15 @@ fn count_kmers(fasta_file: &str, k: usize) -> HashMap<String, usize> {
     kmer_counts
 }
 
+fn find_unique_kmers(kmer_vec: &Vec<String>, kmer_counts: &HashMap<String, usize>, file: &mut File) -> Result<(), Error> {
+    for kmer in kmer_vec {
+        let count = kmer_counts[kmer];
+        match writeln!(file, "{}\t{}\t{}", kmer, 0, count) {
+            Ok(_) => {},
+            Err(e) => {
+                return Err(e);
+            }
+        };
+    }
+    Ok(())
+}
